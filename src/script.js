005e96314b216904c4d5e671ad7345ccf6fa5ee0
mubeel <muhammadmdgh1@gmail.com>
2025-08-25 11:59:10 +0300

    creating teo files of html collages and js of them

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+const arabicSection = document.querySelector('.btn1');
+const generalCollages = document.querySelector('.btn2');
+
+arabicSection.addEventListener('click', () => {
+    window.location.href = './src/arabic-section.html';
+});
+
+generalCollages.addEventListener('click', ()=>{
+    window.location.href = './src/general-collages.html';
+});
+
